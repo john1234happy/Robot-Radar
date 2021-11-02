deee97e6456d330639213e2caf6c8ab4bb7ad028
Zachary Baker <zbaker@oswego.edu>
2021-11-02 11:49:00 -0400

    Update dataInterpreter.java

--- dataInterpreter.java
+++ dataInterpreter.java
@@ -1,24 +1,37 @@
 import java.util.LinkedList;
-import org.json.JSONObject;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 
 class dataInterpreter {
-    static LinkedList<Dot> ll = new LinkedList<Dot>();
+    private static LinkedList<Dot> DotList = new LinkedList<Dot>();
     // this will be a ref to the linked list somewhere else later, but I'm making it here for now
 
     public static void main(String[] args) {
         parseJSON("{\"Dot\":{\"distance\":78.45,\"angle\":26.56}}");
-        printList(ll);
+        printList(DotList);
     }
 
     public static void parseJSON(String s) {
+        // parse JSON - either a new dot, or a previous movement or turn
         // {"Dot":{"distance":78.45,"angle":26.56}}
 
-        JSONObject jo = new JSONObject(s);
+        // TODO: right now it works assuming a distance is passed, but will have to calculate distance travelled using time in future
 
-        if (jo.opt("Dot") != "null") {
-            double dist = Double.parseDouble(jo.getJSONObject("Dot").get("distance").toString());
-            double angle = Double.parseDouble(jo.getJSONObject("Dot").get("angle").toString());
+        String stripped = s.replaceAll("\\s+",""); // remove whitespace
+
+        Pattern pattern = Pattern.compile("[0-9|.]+"); // regex for any number
+        Matcher matcher = pattern.matcher(stripped);
+
+        if (stripped.contains("Dot")) {
+
+            matcher.find(); // we are just taking the first two numbers we find
+
+            double dist = Double.parseDouble(stripped.substring(matcher.start(), matcher.end()));
+
+            matcher.find();
+
+            double angle = Double.parseDouble(stripped.substring(matcher.start(), matcher.end()));
 
             double x = dist*Math.sin(angle);
             double y = dist*Math.cos(Math.toRadians(angle));
@@ -26,31 +39,33 @@ class dataInterpreter {
             addDot(new Dot(x, y));
         }
 
-        else if (jo.opt("movement") != "null") {
-            // todo
+        else if (stripped.contains("movement")) {
+            matcher.find();
+            updateListMovement(Double.parseDouble(stripped.substring(matcher.start(), matcher.end())));
         }
 
-        else if (jo.opt("angle") != "null") {
-            // todo
+        else if (stripped.contains("angle")) {
+            matcher.find();
+            updateListAngle(Double.parseDouble(stripped.substring(matcher.start(), matcher.end())));
         }
     }
 
-    public static void updateListMovement(double dist) { 
+    private static void updateListMovement(double dist) { 
         // update list with (forward or backward) distance moved 
-        for (Dot d : ll) {
+        for (Dot d : DotList) {
             d.update(d.getx(), d.gety() - dist);
             }      
     }
 
-    public static void updateListAngle(double angle) { 
+    private static void updateListAngle(double angle) { 
         // update list with angle turned in degrees
-        for (Dot d : ll) {
+        for (Dot d : DotList) {
             d.update(d.getx() * Math.cos(Math.toRadians(angle)) + d.gety() * Math.sin(Math.toRadians(angle)), 
                 -d.getx() * Math.sin(Math.toRadians(angle)) + d.gety() * Math.cos(Math.toRadians(angle)));
             }      
     }
 
-    public static void printList(LinkedList<Dot> ll) {
+    private static void printList(LinkedList<Dot> ll) { // prints dot list 
         for (int i = 0; i < ll.size(); i++) {
             Dot d = ll.get(i);
             System.out.println("Dot " + i + " -- x: " + d.getx() + ", y: " + d.gety());
@@ -58,29 +73,27 @@ class dataInterpreter {
         System.out.println();
     }
 
-    public static void addDot(Dot d) { 
-        ll.add(d);
+    public static LinkedList<Dot> getList() {
+        // returns dot list 
+        return DotList;
+    }
+
+    private static void addDot(Dot d) { 
+        // adds dot to dot list 
+        DotList.add(d);
     }
 }
 
 
-class Dot {
-    double x; 
-    double y;
 
-    public Dot(double x, double y) {
-        this.x = x; this.y = y;
-    }
 
-    public void update(double newx, double newy) { 
-        x = newx; y = newy;
-    }
 
-    public double getx() {
-        return x;
-    }
 
-    public double gety() {
-        return y;
-    }
-}
+
+
+
+
+
+
+
+

3e836ac73070f0a53b27c63c700ff67ceae6c6f5
john1234happy <john1234happy@gmail.com>
2021-12-01 14:16:18 -0500

    Ustreamer start sript thats called by the server

--- ustreamer.sh
+++ ustreamer.sh
@@ -1,1 +1,4 @@
-
+#!/bin/bash
+ip4="$(/sbin/ip -o -4 addr list wlan0 | awk '{print $4}' | cut -d/ -f1)"
+echo $ip4
+/home/pi/test/Robot-Radar/ustreamer/ustreamer --device=/dev/video0 -r 1080x720 --flip-vertical=1 --host=$ip4 --port=8080
